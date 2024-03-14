9353e570c17d85c13035d347a782057c513441ea
Steve Gazzo <stevegazzojr@gmail.com>
2024-03-13 20:33:57 -0400

    Add EventInstanceRepository.findByUserId

--- src/main/java/com/projectdave/habitrack/controller/EventController.java
+++ src/main/java/com/projectdave/habitrack/controller/EventController.java
@@ -4,7 +4,6 @@ import com.projectdave.habitrack.model.EventInstance;
 import com.projectdave.habitrack.model.EventModel;
 import com.projectdave.habitrack.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/projectdave/habitrack/repository/EventInstanceRepository.java
+++ src/main/java/com/projectdave/habitrack/repository/EventInstanceRepository.java
@@ -3,5 +3,9 @@ package com.projectdave.habitrack.repository;
 import com.projectdave.habitrack.model.EventInstance;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface EventInstanceRepository extends MongoRepository<EventInstance, String> {
+
+    List<EventInstance> findByUserId(String userId);
 }
